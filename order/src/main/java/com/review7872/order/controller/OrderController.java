7627d659d3b1d896ed886ccb86a4ccc3281374ba
Review7872 <fadai031027@gmail.com>
2023-11-17 12:16:07 +0800

    car组件测试完成

--- order/src/main/java/com/review7872/order/controller/OrderController.java
+++ order/src/main/java/com/review7872/order/controller/OrderController.java
@@ -2,17 +2,18 @@ package com.review7872.order.controller;
 
 import com.review7872.order.pojo.Order;
 import com.review7872.order.service.OrderService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/order")
-@Slf4j
 public class OrderController {
     @Autowired
     private OrderService orderService;
@@ -38,7 +39,6 @@ public class OrderController {
     @PostMapping("/insert")
     @CacheEvict(value = "order", allEntries = true)
     public long insert( Long cardId,Long carId,String seatId ) {
-        log.info(new StringBuffer().append(cardId).append("下单成功，车次是").append(carId).append("，座位是").append(seatId).toString());
         return orderService.insertOrder(cardId, carId, seatId);
     }
 
